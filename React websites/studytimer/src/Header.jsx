094e7a55242c3666118ffdc414ce6f6b61bc94b6
Kizuroy <32106420+kizuroy@users.noreply.github.com>
2023-10-09 19:42:31 +0200

    added music

--- React websites/studytimer/src/Header.jsx
+++ React websites/studytimer/src/Header.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import musicFile from './Music/music.mp3';
 
 function Header() {
+    const [audio] = useState(new Audio(musicFile));
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const playMusic = () => {
+        audio.play();
+        setIsPlaying(true);
+    };
+
+    const pauseMusic = () => {
+        audio.pause();
+        setIsPlaying(false);
+    };
+
+    const stopMusic = () => {
+        audio.pause();
+        audio.currentTime = 0;
+        setIsPlaying(false);
+    };
+
     return (
         <header className="app-header">
-            <h1 className="site-name">Study helper</h1>
+            <h1 className="site-name">Study Helper</h1>
             <div className="auth-buttons">
+                {isPlaying ? (
+                    <>
+                    <button className="music-button" onClick={pauseMusic}>
+                        Pause
+                    </button>
+                    <button className="music-button" onClick={stopMusic}>
+                        Stop
+                    </button>
+                    </>
+                    ) : (
+                        <button className="music-button" onClick={playMusic}>
+                            Play
+                        </button>
+                        )}
                 <button className="login-button">Log In</button>
                 <button className="register-button">Register</button>
             </div>
