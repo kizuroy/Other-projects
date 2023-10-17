105597b0944fec198f0d6e837bb0c9d6676b641e
Alex Veljak <32106420+kizuroy@users.noreply.github.com>
2023-10-17 16:52:48 +0200

    updates

--- React websites/studytimer/src/Header.jsx
+++ React websites/studytimer/src/Header.jsx
@@ -3,48 +3,48 @@ import './Header.css';
 import musicFile from './Music/music.mp3';
 
 function Header() {
-    const [audio] = useState(new Audio(musicFile));
-    const [isPlaying, setIsPlaying] = useState(false);
+  const [audio] = useState(new Audio(musicFile));
+  const [isPlaying, setIsPlaying] = useState(false);
 
-    const playMusic = () => {
-        audio.play();
-        setIsPlaying(true);
-    };
+  const playMusic = () => {
+    audio.play();
+    setIsPlaying(true);
+  };
 
-    const pauseMusic = () => {
-        audio.pause();
-        setIsPlaying(false);
-    };
+  const pauseMusic = () => {
+    audio.pause();
+    setIsPlaying(false);
+  };
 
-    const stopMusic = () => {
-        audio.pause();
-        audio.currentTime = 0;
-        setIsPlaying(false);
-    };
+  const stopMusic = () => {
+    audio.pause();
+    audio.currentTime = 0;
+    setIsPlaying(false);
+  };
 
-    return (
-        <header className="app-header">
-            <h1 className="site-name">Study Helper</h1>
-            <div className="auth-buttons">
-                {isPlaying ? (
-                    <>
-                    <button className="music-button" onClick={pauseMusic}>
-                        Pause
-                    </button>
-                    <button className="music-button" onClick={stopMusic}>
-                        Stop
-                    </button>
-                    </>
-                    ) : (
-                        <button className="music-button" onClick={playMusic}>
-                            Play Music
-                        </button>
-                        )}
-                <button className="login-button">Log In</button>
-                <button className="register-button">Register</button>
-            </div>
-        </header>
-        );
+  return (
+    <header className="app-header">
+      <h1 className="site-name">Study Helper</h1>
+      <div className="auth-buttons">
+        {isPlaying ? (
+          <>
+            <button className="music-button" onClick={pauseMusic}>
+              Pause
+            </button>
+            <button className="music-button" onClick={stopMusic}>
+              Stop
+            </button>
+          </>
+        ) : (
+          <button className="music-button" onClick={playMusic}>
+            Play Music
+          </button>
+        )}
+        <button className="login-button">Log In</button>
+        <button className="register-button">Register</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
