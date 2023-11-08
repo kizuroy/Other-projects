5e5ceb957ed057999dda096de3d56994c1e60a7c
Alex Veljak <32106420+kizuroy@users.noreply.github.com>
2023-11-08 18:08:18 +0100

    finished project

--- Apps/Better notes/convex/auth.config.js
+++ Apps/Better notes/convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://perfect-vulture-35.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
