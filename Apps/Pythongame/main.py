7f7f12cc93bd4ac1284d244a03ef251b2d89a07c
Alex Veljak <32106420+kizuroy@users.noreply.github.com>
2023-11-26 14:07:10 +0100

    Update main.py

--- Apps/Pythongame/main.py
+++ Apps/Pythongame/main.py
@@ -16,9 +16,24 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name):
+    image = pygame.image.load(join('assets', 'Bacground', name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = [i * width, j * height]
+            tiles.append(pos)
+    
+    return tiles, image
+
+
+
 def main(window):
     clock = pygame.time.Clock()
-
+    background = get_background('Brown.png')
+    
     run = True
     while run:
         clock.tick(FPS)
