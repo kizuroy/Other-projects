0a1b20b1d2e36ec3fd56b021f3986bea8acff7e9
Alex Veljak <32106420+kizuroy@users.noreply.github.com>
2023-11-27 07:43:49 +0100

    Update main.py

--- Apps/Pythongame/main.py
+++ Apps/Pythongame/main.py
@@ -8,7 +8,7 @@ pygame.init()
 
 pygame.display.set_caption('Platformer')
 
-BG_COLOUR = (255, 255, 255)
+
 WIDTH, HEIGHT = 1000, 800
 
 FPS = 120
@@ -17,22 +17,27 @@ PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def get_background(name):
-    image = pygame.image.load(join('assets', 'Bacground', name))
+    image = pygame.image.load(join('assets', 'Background', name))
     _, _, width, height = image.get_rect()
     tiles = []
 
     for i in range(WIDTH // width + 1):
         for j in range(HEIGHT // height + 1):
-            pos = [i * width, j * height]
+            pos = (i * width, j * height)
             tiles.append(pos)
     
     return tiles, image
 
-
+def draw(window, background, bg_image):
+    
+    for tile in background:
+        window.blit(bg_image, tile)
+    
+    pygame.display.update()
 
 def main(window):
     clock = pygame.time.Clock()
-    background = get_background('Brown.png')
+    background, bg_image = get_background('Brown.png')
     
     run = True
     while run:
@@ -42,6 +47,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
     
     pygame.quit
     quit()
